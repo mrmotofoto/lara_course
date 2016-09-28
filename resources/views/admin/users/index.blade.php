dee319323ddb800370c24f24abf9384067dacf03
Rich <richardstiehm@gmail.com>
2016-09-28 07:19:23 -0400

    Added ternary to is active

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -23,6 +23,7 @@
             <td>{{$user->id}}</td>
             <td>{{$user->name}}</td>
             <td>{{$user->email}}</td>
+            {{--<td>{{$user->role->name}}</td>--}}
             @if($user->role_id == 1)
                 <td>Admin</td>
                 @elseif($user->role_id == 2)
@@ -32,13 +33,7 @@
                     @endelse
                 @endelseif
             @endif
-            @if($user->is_active == 1)
-                <td>Active</td>
-                @else
-                <td>InActive</td>
-                @endelse
-            @endif
-            {{--<td>{{$user->role->name}}</td>--}}
+            <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
             <td>{{$user->created_at->diffForHumans()}}</td>
             <td>{{$user->updated_at->diffForHumans()}}</td>
         </tr>
