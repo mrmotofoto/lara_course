572be8a3bdc636148dfc3c2d0930f08f837931d4
Rich <richardstiehm@gmail.com>
2016-10-04 15:09:40 -0400

    Working Photo Add Delete

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -17,6 +17,14 @@
                  <td>{{$photo->id}}</td>
                  <td><img height="50" src="{{$photo->file}}" alt=""></td>
                  <td>{{$photo->created_at ? $photo->created_at : 'no date'}}</td>
+                   <td>
+                       {!! Form::open(['method'=>'DELETE', 'action' => ['AdminMediaController@destroy', $photo->id]]) !!}
+                       <div class="form-group">
+                           {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                       </div>
+                       {!! Form::close() !!}
+                   </td>
+
                </tr>
              @endforeach
              </tbody>
