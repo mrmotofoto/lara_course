1e657c852c2288e7a9f6d5f32d69c1098f8c1934
Rich <richardstiehm@gmail.com>
2016-10-05 15:07:46 -0400

    Working Comments For A Specific Post

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
+use App\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -45,7 +46,11 @@ class PostCommentsController extends Controller
 
     public function show($id)
     {
-        //
+
+        $post = Post::findOrFail($id);
+        $comments = $post->comments;
+        //dd($comments);
+        return view('admin.comments.show', compact('comments', 'post'));
     }
 
 
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -11,6 +11,7 @@
     @if(Session::has('deleted_post'))
         <p class="bg-danger">{{session('deleted_post')}}</p>
     @endif
+    <h1>Posts</h1>
   @if($posts)
         <table class="table">
             <thead>
@@ -34,6 +35,8 @@
                 <td><img style="width:50px" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
                 <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->title}}</a></td>
                 <td>{{str_limit($post->body, 30)}}</td>
+                <td><a href="{{route('home.post', $post->id)}}">View Post</a></td>
+                <td><a href="{{route('admin.comments.show', $post->id)}}">View Comments</a></td>
                 <td>{{$post->created_at->diffForHumans()}}</td>
                 <td>{{$post->updated_at->diffForHumans()}}</td>
             </tr>
